9c18dff0e6175b4198eb7fa2d1016358fc018871
Tarang Hirani <tarang9211@gmail.com>
2016-11-12 02:04:34 +0530

    trying to get data to verify if nsurlsession is working

--- RSSFeed/ViewController.swift
+++ RSSFeed/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
 
+  private var xmlParser = XMLParser()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.getRSSFeed()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+  }
+  
+  private func getRSSFeed() {
+    let url = URL(string: "https://www.cnet.com/rss/news/")!
+    let urlRequest = URLRequest(url: url)
+    let session = URLSession(configuration: .default)
+    
+    let task = session.dataTask(with: urlRequest) { (data, response, error) in
+      DispatchQueue.main.async {
+        print(data!)
+      }
+    }
+    
+    task.resume()
+    
   }
 
 
