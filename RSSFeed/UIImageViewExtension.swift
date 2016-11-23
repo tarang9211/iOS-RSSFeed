b06ade05964f9d9939f72d1e3b09d5dc0c4298d6
Tarang Hirani <tarang9211@gmail.com>
2016-11-23 11:50:34 +0530

    fixing imageview extension

--- RSSFeed/UIImageViewExtension.swift
+++ RSSFeed/UIImageViewExtension.swift
@@ -16,13 +16,26 @@ extension UIImageView {
     
     func downloadImage(link: String) {
         
+        self.image = UIImage(named: "placeholder")
+        
         let url =  URL(string: link)!
         let urlRequest = URLRequest(url: url)
         let session = URLSession(configuration: .default)
         
         let task = session.dataTask(with: urlRequest) { (data, response, error) in
             
+            guard let httpResponse = response as? HTTPURLResponse else {
+                return
+            }
+            
+            if (httpResponse.statusCode == 200) {
+                if let imgData = data {
+                    let img = UIImage(data: imgData)
+                    
+                }
+            }
         }
+        
         
         task.resume()
     }
